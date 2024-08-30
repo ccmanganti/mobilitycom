95d43033ad3fd14e5f99b227a668650a851a1b7c
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2024-08-30 12:28:27 +0800

    Modified User Registration

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Custom Imports
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\GlovesController;
+use App\Http\Controllers\ActionsController;
+use App\Http\Controllers\ReadingsController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
-Route::get('/student/{id?}', function (string $id = 'sample') {
-    return $id;
-});
-
+Route::resource('users', UserController::class);
+Route::resource('gloves', GlovesController::class);
+Route::resource('actions', ActionsController::class);
+Route::resource('readings', ReadingsController::class);
